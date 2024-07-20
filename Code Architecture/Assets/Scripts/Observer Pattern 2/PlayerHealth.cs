9f223494b6efb130fd37502cedd56ec73fa0bee2
Ugur Karabulut <thekarabulut95@gmail.com>
2024-07-20 12:47:45 +0300

    feat: Generic observer

--- Code Architecture/Assets/Scripts/Observer Pattern 2/PlayerHealth.cs
+++ Code Architecture/Assets/Scripts/Observer Pattern 2/PlayerHealth.cs
@@ -5,10 +5,12 @@ namespace CodeArchitecture.Observer_2
     public class PlayerHealth : MonoBehaviour
     {
         public Observer<int> Health = new Observer<int>(100);
+        public Observer<bool> IsAlieve = new Observer<bool>(true);
 
         void Start()
         {
             Health.Invoke();
+            IsAlieve.Invoke();
         }
 
         void Update()
@@ -16,6 +18,20 @@ namespace CodeArchitecture.Observer_2
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 Health.Value += 10;
+                if (!IsAlieve.Value)
+                {
+                    IsAlieve.Value = true;
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.RightShift))
+            {
+                if (Health.Value > 0)
+                    Health.Value -= 10;
+                if (Health.Value <= 0)
+                {
+                    IsAlieve.Value = false;
+                }
             }
         }
     }
---
+++ Code Architecture/Assets/Scripts/Observer Pattern 2/PlayerStateDisplay.cs
@@ -0,0 +1,20 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace CodeArchitecture.Observer_2
+{
+    public class PlayerStateDisplay : MonoBehaviour
+    {
+        TMP_Text _stateText;
+
+        void Awake()
+        {
+            _stateText = GetComponent<TMP_Text>();
+        }
+
+        public void UpdateStateDisplay(bool isAlive)
+        {
+            _stateText.text = isAlive ? "Alive" : "Dead";
+        }
+    }
+}
