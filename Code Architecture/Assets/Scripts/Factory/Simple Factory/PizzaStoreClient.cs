f27175861119945b4a029701b406126496821ab7
Ugur Karabulut <thekarabulut95@gmail.com>
2024-07-21 23:21:27 +0300

    feat: Odin implemented

---
+++ Code Architecture/Assets/Scripts/Factory/Simple Factory/PizzaStoreClient.cs
@@ -0,0 +1,15 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace CodeArchitecture.SimpleFactory
+{
+    public class PizzaStoreClient : MonoBehaviour
+    {
+        [Button]
+        public void OrderPizza(PizzaTypes pizzaTypes)
+        {
+            var pizzaStore = new PizzaStore();
+            pizzaStore.OrderPizza(pizzaTypes);
+        }
+    }
+}
